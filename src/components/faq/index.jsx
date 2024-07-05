5e50441f373ae54a6e97c840b0d933950a82e776
Susu640 <sbsbadroo@gmail.com>
2024-07-05 18:36:42 +0200

    Ultimos toques, bom final de semana

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -8,7 +8,7 @@ export const FaqComponents = ({pergunta, resposta}) => {
     <>
       <div className="py-1 ">
       <button
-          className="flex justify-between w-full bg-sky-blue-200 text-white  py-2 px-4 rounded-md mb-2 hover:bg-black focus:outline-none focus:ring-2 focus:ring-cream-100"
+          className="flex justify-between w-full bg-sky-blue-200 text-white  py-2 px-4 rounded-md mb-2 hover:bg-black hover:text-orange focus:outline-none focus:ring-2 focus:ring-cream-100"
           onClick={() => setAberto(!aberto)}
         >
           <span> {pergunta}</span>
@@ -17,7 +17,7 @@ export const FaqComponents = ({pergunta, resposta}) => {
 
         <div
           className={`overflow-hidden transition-all duration-300 ease-in-out ${aberto ? "max-h-full opacity-100 transform translate-y-0" : "max-h-0 opacity-0 transform -translate-y-full"}`}>
-          <div className="bg-sky-blue-100 p-2">{resposta}</div>
+          <div className="bg-sky-blue-100 p-2 hover:bg-orange/50 ">{resposta}</div>
         </div>
       </div>
     </>
