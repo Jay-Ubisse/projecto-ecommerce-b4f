23f69f060da8c557b8f7510406dcdc40b3345e6d
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-05 16:32:19 +0200

    estrutura do faq

---
+++ src/components/faq/index.jsx
@@ -0,0 +1,18 @@
+
+
+export const FaqComponents = () => {  
+  return (
+    <>
+      <div>
+        <button>
+          <span> Como faço para criar uma conta? </span>
+          <span>+</span>
+        </button>
+
+        <div>
+          <div>Geralmente, há um botão "Registrar" ou "Criar Conta" na página inicial. Você precisará fornecer seu nome, endereço de e-mail e criar uma senha.</div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -1,10 +1,14 @@
+import { FaqComponents } from "../components/faq";
 import { Header } from "../components/header";
 
 export const FAQ = () => {
   return (
     <>
       <Header />
-      <main>Perguntas frequentes</main>
+      <main>
+        <h1>Perguntas frequentes</h1>
+          <FaqComponents/>
+      </main>
     </>
   );
 };
