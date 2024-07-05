b8e706d777db1ce78692491addeab5e455b6d967
InacioMugana <inaciomugaua@gmail.com>
2024-07-05 16:49:37 +0200

    update:create component

---
+++ src/components/emailCard.jsx
@@ -0,0 +1,15 @@
+export const EmailCard = () => {
+  return (
+    <>
+      <div>
+        <h2>E-MAIL</h2>
+        <div>
+          <div>
+            <a href="#">ecommerce@shop.com</a>
+            <a href="#">suportecommerce@shop.com</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
