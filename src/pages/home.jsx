481c7876da2104f2ebbe4b850ab3fe10df288f17
GersonChuquela <gersonjubelito@gmail.com>
2024-07-05 18:34:02 +0200

    creat:sponsor image card

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
 import { Slider } from "../components/slider/slider";
+import { ImageCard } from "../components/image-card/image-card";
 
 export const Home = () => {
   return (
     <>
       <Header />
       <Slider />
+      <ImageCard />
       <Footer />
     </>
   );
