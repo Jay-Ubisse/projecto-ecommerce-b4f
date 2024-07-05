d07cb69ece47a6fd838c459489ffc9636ffcfac6
alexandreMahumane <alexandremahumane69@gmail.com>
2024-07-05 17:15:05 +0200

    adicao do objecto de pergunta

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 
-export const FaqComponents = () => {  
+export const FaqComponents = ({pergunta, resposta}) => {  
   const [aberto, setAberto] = useState(false);
+
   return (
     <>
       <div className="py-2">
@@ -10,16 +11,16 @@ export const FaqComponents = () => {
           className="flex justify-between w-full bg-sky-blue-200 text-white py-2 px-4 rounded-md mb-2 hover:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-blue-500"
           onClick={() => setAberto(!aberto)}
         >
-          <span> Como faço para criar uma conta? </span>
+          <span> {pergunta}</span>
           <span>{aberto ? "-" : "+"}</span>
         </button>   
 
         <div
           className={`overflow-hidden transition-all duration-300 ease-in-out ${aberto ? "max-h-full opacity-100 transform translate-y-0" : "max-h-0 opacity-0 transform -translate-y-full"}`}
         >
-          <div className="bg-sky-blue-100 p-4">Geralmente, há um botão "Registrar" ou "Criar Conta" na página inicial. Você precisará fornecer seu nome, endereço de e-mail e criar uma senha.</div>
+          <div className="bg-sky-blue-100 p-4">{resposta}</div>
         </div>
-      
+      </div>
     </>
   );
 };
--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -1,13 +1,32 @@
-import { FaqComponents } from "../components/faq";
 import { Header } from "../components/header";
+import { FaqComponent } from "../components/faq";
 
 export const FAQ = () => {
+  const faq = [
+    {
+      pergunta: "Como faço para realizar uma compra?",
+      resposta: "Para fazer uma compra, basta navegar até o produto desejado, selecionar a quantidade e clicar no botão 'Comprar'. Em seguida, siga as instruções para finalizar o pedido."
+    },
+    {
+      pergunta: "Quais são as opções de pagamento disponíveis?",
+      resposta: "Aceitamos cartões de crédito (Visa, MasterCard, American Express), boleto bancário e transferência bancária como formas de pagamento."
+    },
+    {
+      pergunta: "Como posso rastrear meu pedido?",
+      resposta: "Assim que seu pedido for enviado, você receberá um e-mail com o número de rastreamento. Você pode usar esse número no site da transportadora para acompanhar a entrega."
+    },
+    {
+      pergunta: "Qual é a política de devolução da loja?",
+      resposta: "Aceitamos devoluções dentro de 30 dias após a compra, desde que o produto esteja em condições adequadas e com a embalagem original."
+    }
+  ];
   return (
     <>
       <Header />
       <main>
-        <h1>Perguntas frequentes</h1>
-          <FaqComponents/>
+        {faq.map(e => <FaqComponent pergunta={e.pergunta} resposta={e.resposta} />)}
+        
+
       </main>
     </>
   );
