26ef18a175651418025a547d72aefd28880eef6d
Susu640 <sbsbadroo@gmail.com>
2024-07-05 16:48:54 +0200

    Configuracao do botao

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 
 
 export const FaqComponents = () => {  
+  const [aberto, setAberto] = useState(false);
   return (
     <>
       <div>
-        <button>
+      <button
+         
+          onClick={() => setAberto(!aberto)}
+        >
           <span> Como faço para criar uma conta? </span>
-          <span>+</span>
-        </button>
+          <span>{aberto ? "-" : "+"}</span>
+        </button>                                                                                                       
 
         <div>
           <div>Geralmente, há um botão "Registrar" ou "Criar Conta" na página inicial. Você precisará fornecer seu nome, endereço de e-mail e criar uma senha.</div>
