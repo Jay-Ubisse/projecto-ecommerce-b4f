748b6418202f2982a9b1cdf9b21c99941f3557e0
unknown <georginadavidcumbe@gmail.com>
2024-07-05 18:21:33 +0200

    ipdate: geor

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,10 +12,16 @@ export const About = () => {
 
       /*  vicente /*
       /*  Jorgina /*
-      <div>
-        <div></div>
-        <div></div>
+      <div className="flex gap-6 text-justify my-4">
+        <div>
+        <h1 className="font-bold text-2xl justify-between">O seu destino de moda onde a elegância encontra a modernidade</h1>
+        </div>
+        <div >
+        <p>Na Kuxonga, somos apaixonados por proporcionar  uma experiência de compra única. Nosso compromisso vai além das peças de roupa que oferecemos; queremos inspirar confiança e expressão pessoal por meio da moda. Desde roupas casuais até looks sofisticados, nossa coleção é cuidadosamente curada para atender aos diversos estilos e ocasiões.</p>
+        <p >Nossa coleção é um reflexo da nossa dedicação em atender a diversos estilos e ocasiões. Das roupas casuais que abraçam a simplicidade à elegância sofisticada que destaca a singularidade, cada peça é cuidadosamente selecionada para enriquecer a jornada da moda de nossos clientes.</p>
+        </div>
       </div>
+      /*  Jorgina /*
       /*  Jorgina /*
 
       
