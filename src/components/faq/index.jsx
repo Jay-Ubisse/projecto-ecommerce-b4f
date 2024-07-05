65cb4b37b30866edbdc44112117e09288eb75b86
Reginaldo16 <Reginaldotaylor16@gmail.com>
2024-07-05 16:56:53 +0200

    estilizacao do button

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -5,14 +5,14 @@ export const FaqComponents = () => {
   const [aberto, setAberto] = useState(false);
   return (
     <>
-      <div>
+      <div className="py-2">
       <button
-         
+          className="flex justify-between w-full bg-sky-blue-200 text-white py-2 px-4 rounded-md mb-2 hover:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-blue-500"
           onClick={() => setAberto(!aberto)}
         >
           <span> Como faço para criar uma conta? </span>
           <span>{aberto ? "-" : "+"}</span>
-        </button>                                                                                                       
+        </button>                                                                                                      
 
         <div>
           <div>Geralmente, há um botão "Registrar" ou "Criar Conta" na página inicial. Você precisará fornecer seu nome, endereço de e-mail e criar uma senha.</div>
