9306061fc9314b90b32e83456a228381321413f4
unknown <georginadavidcumbe@gmail.com>
2024-07-08 09:36:10 +0200

    new

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,7 +19,7 @@ export const About = () => {
         <div>
         <h1 className="font-bold text-2xl justify-between m-4">O seu destino de moda onde a elegância encontra a modernidade</h1>
         </div>
-        <div className="text-justify px-4 lg:px-96 ">
+        <div className="text-justify ">
         <p >Na Kuxonga, somos apaixonados por proporcionar  uma experiência de compra única. Nosso compromisso vai além das peças de roupa que oferecemos; queremos inspirar confiança e expressão pessoal por meio da moda. Desde roupas casuais até looks sofisticados, nossa coleção é cuidadosamente curada para atender aos diversos estilos e ocasiões.</p>
         <p >Nossa coleção é um reflexo da nossa dedicação em atender a diversos estilos e ocasiões. Das roupas casuais que abraçam a simplicidade à elegância sofisticada que destaca a singularidade, cada peça é cuidadosamente selecionada para enriquecer a jornada da moda de nossos clientes.</p>
         </div>
