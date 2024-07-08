c207d293cd4831494af61b28c34c4fba27ee313e
ricardina-ngovene <ngovenericardina27@gmail.com>
2024-07-08 15:35:31 +0200

    card number

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/header";
-
+import { Card } from "../components/card-numer";
 export const Contact = () => {
   return (
     <>
+  
       <Header />
+      <Card/>
       <main>Página dos contactos</main>
     </>
   );
