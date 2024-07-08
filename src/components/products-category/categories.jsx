d6bd43b22e1dc8786db3cc1856be795200e106d4
Achirrime <alcindachirrime@gmail.com>
2024-07-08 15:12:25 +0200

    create product categories

---
+++ src/components/products-category/categories.jsx
@@ -0,0 +1,42 @@
+export const Categories = () => {
+    return (
+      <div className="flex flex-col max-w-[1400px] m-auto">
+        <h1 className="m-auto font-bold py-4 ">COMPRE POR CATEGORIA</h1>
+       <div className="flex items-center gap-8 m-auto py-4 font-bold">
+        <div>
+        <img src="./assets/categoria-mulher.jpg" alt="" className="rounded-full w-60 h-60 py-2" />
+        <p className="ml-10">Roupa Para Mulher</p>
+        </div>
+        <div>
+        <img src="./assets/categoria-homens.jpg" alt="" className="rounded-full w-60 h-60 py-2" />
+        <p className="ml-10">Roupa Para Homem</p>
+        </div>
+        <div> 
+        <img src="./assets/categoria-bebes.jpg" alt="" className="rounded-full w-60 h-60 py-2"/>
+        <p className="ml-14">Bebés e Criança</p>
+        </div>
+        <div>
+        <img src="./assets/categoria-calcados.jpg" alt="" className="rounded-full w-60 h-60 py-2" />
+        <p className="ml-16">Calçados</p>
+        </div>
+        <div>
+        <img src="./assets/categoria-acessorios.jpg" alt="" className="rounded-full w-60 h-60 py-2" />
+        <p className="ml-16">Acessórios</p>
+        </div>
+        </div>
+      </div>
+      /*
+       <div>
+        <h3>COMPRA POR CATEGORIA</h3>
+       </div>
+        <div>
+       <img src="https://i.pinimg.com/originals/dc/52/07/dc5207236a72fdb0282d821103c39183.jpg" alt="" />
+       <img src="https://th.bing.com/th/id/R.5cb901a875dde8742650c02413f61004?rik=L0X5RA5AepMVnw&pid=ImgRaw&r=0" alt="" />
+       <img src="https://th.bing.com/th/id/R.2539a4e8f3516df521d536434800f1ec?rik=pJW9lx7jBnEAYw&pid=ImgRaw&r=0" alt="" />
+       <img src="https://http2.mlstatic.com/sapato-social-masculino-couro-derby-oxford-brogue-bico-fino-D_NQ_NP_714343-MLB28582975758_112018-F.jpg" alt="" />
+        </div>
+        */
+      
+    );
+  };
+  
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
 import { Slider } from "../components/slider/slider";
+import { Categories } from "../components/products-category/categories";
 import { ImageCard } from "../components/image-card/image-card";
 
 export const Home = () => {
@@ -8,6 +9,7 @@ export const Home = () => {
     <>
       <Header />
       <Slider />
+      <Categories />
       <ImageCard />
       <Footer />
     </>
