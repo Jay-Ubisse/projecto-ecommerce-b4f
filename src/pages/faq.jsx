c24b720a18d8d7ebd0894a0385d30c1694e3ec21
Susu640 <sbsbadroo@gmail.com>
2024-07-08 11:34:12 +0200

    fotos

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -23,14 +23,21 @@ export const FAQ = () => {
   return (
     <>
       <Header />
-      <main>
-        <div  className="mt-28">
-          <h1 className="flex justify-center font-bold text-2xl w-full text-sky-blue-200 ">Perguntas Mais Frequentes</h1>
+      <main >
+        <div  className="mt-20">
+          <h1 className="flex justify-center font-bold text-2xl w-full text-sky-blue-200  ">Perguntas Mais Frequentes</h1>
           <div className="px-[250px]  mt-16 ">    
           {faq.map(e => <FaqComponents className="flex justify-center " pergunta={e.pergunta} resposta={e.resposta} />)}
         </div>
         </div>
+        <div className="flex justify-center items-center  relative  rad max-w-full h-80 mt-28 ">
+          <img src="https://blogdodia.com.br/wp-content/uploads/2018/03/courier-signing-document-H6YTX37-min-1-1568x1046.jpg" alt="" className="w-96 rounded-lg"  />
+        <img src="https://www.completecontroller.com/wp-content/uploads/Business-During-the-Summer-Complete-Controller.jpg" alt=""  className="w-96 rounded-md ml-4" />
+      <img src="https://th.bing.com/th/id/R.9afdfb0eb36bcadb8f485875dcb9d718?rik=NlnJ37SL7NokzA&pid=ImgRaw&r=0" alt="" className="w-96 rounded-md ml-4" />
+      </div>
+        <div ><a hre><img src="" alt="" /></a></div>
       </main>
+      
     </>
   );
 };
