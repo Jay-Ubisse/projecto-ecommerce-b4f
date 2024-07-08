e5adcf15a4e3aa4740a17dc7d7f63be7ab619979
InacioMugana <inaciomugaua@gmail.com>
2024-07-08 09:41:36 +0200

    card email

--- src/components/emailCard.jsx
+++ src/components/emailCard.jsx
@@ -1,15 +1,21 @@
+import { EnvelopeSimple } from "@phosphor-icons/react";
 export const EmailCard = () => {
   return (
-    <>
-      <div>
-        <h2>E-MAIL</h2>
-        <div>
-          <div>
-            <a href="#">ecommerce@shop.com</a>
-            <a href="#">suportecommerce@shop.com</a>
-          </div>
+    <div className=" bg-black bg-opacity-87 text-white p-2 text-center space-y-3 h-15 max-w-sm mx-auto shadow-2xl rounded-3xl">
+      <div className="icon ">
+        <div className="p-2 h-12 w-12 ring-4 ring-white pt-1   m-auto">
+          <EnvelopeSimple size={32} />
         </div>
       </div>
-    </>
+
+      <h2 className="font-bold">E-MAIL</h2>
+      <div>
+        <div>
+          <a href="#">Kuxonga@shop.com</a>
+          <br />
+          <a href="#">suporteKuxonga@shop.com</a>
+        </div>
+      </div>
+    </div>
   );
 };
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,10 @@
 import { Header } from "../components/header";
-
+import { EmailCard } from "../components/emailCard";
 export const Contact = () => {
   return (
     <>
       <Header />
+      <EmailCard />
       <main>Página dos contactos</main>
     </>
   );
