1dc3045fc495c5c9db7059673e2e1cb08a66c24e
unknown <anchacatarina@gmail.com>
2024-07-08 15:37:44 +0200

    featured products

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import { Header } from "../components/header";
 import { Footer } from "../components/footer";
 import { Slider } from "../components/slider/slider";
 import { ImageCard } from "../components/image-card/image-card";
+import { Featured } from "../components/Featured/featured";
 
 export const Home = () => {
   return (
@@ -10,6 +11,7 @@ export const Home = () => {
       <Slider />
       <ImageCard />
       <Footer />
+      <Featured/>
     </>
   );
 };
