a629477f02bc32cecb62909318436242351597fb
jessyduarte82 <jessyduarte82@gmail.com>
2024-07-08 15:27:57 +0200

    fix:remove contact folder inside header folder

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -7,6 +7,14 @@ export const Contact = () => {
     <>
       <Header />
       <main>
+        <div className="flex justify-center max-w-flex justify-center w-full max-w-96 flex-col m-auto px-4 py-4g flex-col m-auto items-center">
+          <h2 className="">Contactos</h2>
+          <p className="">
+            Venha aproveitar grandes novidades na nossa loja através do nossos
+            serviços abaixo.
+          </p>
+        </div>
+
         <SocialMediaCard />
         <EmailCard />
       </main>
