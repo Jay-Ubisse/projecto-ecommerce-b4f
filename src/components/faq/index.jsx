d99c13db02bbbea76263819f97473bfda2bb25c6
Badrú <sbsbadroo@gmail.com>
2024-07-09 10:24:00 +0200

    new page error

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -11,13 +11,13 @@ export const FaqComponents = ({question, answer}) => {
           className="flex justify-between w-full bg-sky-blue-200 text-white  py-2 px-4 rounded-md mb-2 hover:bg-black hover:text-orange focus:outline-none focus:ring-2 focus:ring-cream-100"
           onClick={() => setOpen(!open)}
         >
-          <span> {answer}</span>
+          <span> {question}</span>
           <span>{open ? "-" : "+"}</span>
         </button>   
 
         <div
           className={`overflow-hidden transition-all duration-300 ease-in-out ${open ? "max-h-full opacity-100 transform translate-y-0" : "max-h-0 opacity-0 transform -translate-y-full"}`}>
-          <div className="bg-sky-blue-100 p-2 hover:bg-orange/50 ">{question}</div>
+          <div className="bg-sky-blue-100 p-2 hover:bg-orange/50 ">{answer}</div>
         </div>
       </div>
     </>
--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -6,7 +6,7 @@ export const ErrorPage = () =>{
     return (
         <div>
             <Header/>
-            <main >
+            <main className="">
                 <div className="flex justify-center py-4">
                 <div className="w-1/5 border-b-black">
                 <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -14,8 +14,11 @@ export const ErrorPage = () =>{
                   Quasi voluptate similique tempore at aspernatur placeat veritatis nulla,
                    error labore repellat eligendi tempora!</p>
 
-                <button className="text-sky-blue-200"><Link to ='/'>Home</Link></button>
-                </div>
+
+                   <div>
+                   <Link to ='/'> <button className="text-white text-sm border-black border-spacing-4 rounded-2xl bg-black/70 w-40 h-9 hover:bg-sky-blue-200 hover:transition-all duration-700 ease-in-out hover:w-44 m- ">Back To Homepage</button></Link>
+                   </div>
+                   </div>
                 </div>
             </main>
         </div>
