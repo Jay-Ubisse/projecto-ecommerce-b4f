3d38bf901db246f7c6a6aaab5ad62795c7cab6dd
Reginaldo16 <Reginaldotaylor16@gmail.com>
2024-07-09 11:23:46 +0200

    adicionando imgError

--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -1,26 +1,32 @@
-import { Link } from "react-router-dom"
-import { Header } from "../components/header"
+import { Link } from "react-router-dom";
+import { Header } from "../components/header";
+import imgError from "../components/faq/img/img-erro.png"
 
+export const ErrorPage = () => {
+  return (
+    <div>
+      <Header />
+      <main className="main">
+        <div className="flex justify-center items-center  flex-col mt-72">
+        <img src={imgError} alt="Logo da minha aplicação" className="w-[20%] " />
+          <div className=" border-b-black flex justify-center flex-col">
+            <strong className="text-8xl text-orange">OOPS</strong>
+                <p className="text-center text-2xl my-5">
+                
+                Página não encontrada
+                </p>
 
-export const ErrorPage = () =>{
-    return (
-        <div>
-            <Header/>
-            <main className="">
-                <div className="flex justify-center py-4">
-                <div className="w-1/5 border-b-black">
-                <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                 Sapiente at impedit eum corporis tenetur officia aspernatur.
-                  Quasi voluptate similique tempore at aspernatur placeat veritatis nulla,
-                   error labore repellat eligendi tempora!</p>
-
-
-                   <div>
-                   <Link to ='/'> <button className="text-white text-sm border-black border-spacing-4 rounded-2xl bg-black/70 w-40 h-9 hover:bg-sky-blue-200 hover:transition-all duration-700 ease-in-out hover:w-44 m- ">Back To Homepage</button></Link>
-                   </div>
-                   </div>
+                <div className="flex justify-center items-center">
+                    <Link to="/">
+                        {" "}
+                        <button className="text-white text-sm border-black border-spacing-4 rounded-2xl bg-black/70 w-40 h-9 hover:bg-sky-blue-200 hover:transition-all duration-700 ease-in-out hover:w-44 m- ">
+                        Back To Homepage
+                        </button>
+                    </Link>
                 </div>
-            </main>
+          </div>
         </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
