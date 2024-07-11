4ba2fbfe17b52542b2063aa066bbb85730dac055
macjjane <maidajane83@gmail.com>
2024-07-11 17:58:47 +0200

    update

--- src/components/ShoppingCard/ShoppingCard.jsx
+++ src/components/ShoppingCard/ShoppingCard.jsx
@@ -1,45 +1,39 @@
-import { LockKey, X } from "@phosphor-icons/react"
+import { LockKey, X } from "@phosphor-icons/react";
+import { useState } from "react";
+
+export const ShoppingCard = ({ open, onClose }) => {
 
 
-export const ShoppingCard = () => {
-    const styleDiv = 'flex justify-between items-center'
-    return (
-
-        <div   className="bg-black/45 absolute z-10  ">
-            <div className="flex flex-col gap-2 p-2 bg-white  text-black w-[22%] 2xl:mt-[844px] 2xl:h-screen  2xl:ml-[1420px] ">
-            <div className="flex   justify-between gap-44 items-center">
-                <h1 className="font-bold text-xl">A minha carrinha </h1>
-                <X size={20} />
-            </div>
-            <p className="text-zinc-400">A sua carrinha está vazia. Adicione produtos à carrinha.</p>
-            <div className="border-t-2 border-b-2 pt-2 pb-2">
-
-                <div className="flex justify-between">
-                    <span>Subtotal</span>
-                    <span>0 MT</span>
-
-                </div>
-                <div className="flex justify-between">
-                    <span>Taxa de entrega</span>
-                    <span>0 MT</span>
-
-                </div>
-                <div className="flex justify-between">
-                    <span>Total</span>
-                    <span>0 MT</span>
-
-                </div>
-
-              
-            </div> 
-            <button className="bg-sky-blue-200 rounded-md text-white  font-semibold hover:bg-sky-blue-100 hover:text-black cursor-pointer hover:duration-700">Checkout</button>
-            <div className="flex gap-2 justify-center">
-                <span className="mt-2"> <LockKey/></span>
-                <p className="text-black">Compra segura</p>
-
-            </div>
-        </div> 
+  return (
+    <div className={`bg-black/45 bg-cover absolute z-10 h-screen w-screen mt-[830px] ${open ? "block" : "hidden"}`}>
+      <div className="flex flex-col gap-2 p-2 bg-white text-black w-[22%] 2xl:2xl:h-screen 2xl:ml-[1496px]">
+        <div className="flex justify-between gap-44 items-center">
+          <h1 className="font-bold text-xl">A minha carrinha</h1>
+    
+          <X size={20} onClick={onClose} className="cursor-pointer active:border-2 border-black p-1 active:rounded-md" />
         </div>
-       
-    )
-}
+
+        <p className="text-zinc-400">A sua carrinha está vazia. Adicione produtos à carrinha.</p>
+        <div className="border-t-2 border-b-2 pt-2 pb-2">
+          <div className="flex justify-between">
+            <span>Subtotal</span>
+            <span>0 MT</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Taxa de entrega</span>
+            <span>0 MT</span>
+          </div>
+          <div className="flex justify-between font-semibold">
+            <span>Total</span>
+            <span>0 MT</span>
+          </div>
+        </div>
+        <button className="bg-sky-blue-200 rounded-md text-white font-semibold hover:bg-sky-blue-100 hover:text-black cursor-pointer hover:duration-700">Checkout</button>
+        <div className="flex gap-2 justify-center items-center">
+          <span className="mt-1"> <LockKey /></span>
+          <p className="text-black">Compra segura</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,22 +1,32 @@
 import { ShoppingBag, User } from "@phosphor-icons/react";
 import { NavBar } from "./nav-bar";
 import { ShoppingCard } from "../ShoppingCard/ShoppingCard";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleShoppingBagClick = () => {
+    setIsOpen(!isOpen);
+  };
+  
   return (
-    <header className="flex items-center justify-between bg-sky-blue-200 text-white h-20 px-8 text-lg">
-      <h1 className="text-2xl font-bold border border-white p-2">LOGO</h1>
-      <section className="flex gap-5">
+    <header className="flex items-center justify-between bg-sky-blue-200 text-white h-20 text-lg">
+      <h1 className="text-2xl font-bold border border-white p-2 ml-8">LOGO</h1>
+      <section className="flex gap-5 mr-8">
         <NavBar />
         <div>
           <User size={24} weight="bold" />
         </div>
-        <div>
-          <ShoppingBag size={24} weight="bold" />
+        <div className="flex justify-center cursor-pointer hover:text-red-500 items-center gap-1">
+         
+          <ShoppingBag size={24} weight="bold" onClick={handleShoppingBagClick} />
+          <span className="">0</span>
         </div>
       </section>
 
-      <ShoppingCard/>
+    
+      <ShoppingCard open={isOpen} onClose={() => setIsOpen(false)} />
     </header>
   );
 };
